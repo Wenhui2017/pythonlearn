0a2e409c28f9d1cf50125f76fa78bdcfbef9b5de
Wenhui2017 <928448268@qq.com>
2017-07-20 16:41:30 +0800

    Temperature conversion program

---
+++ Listing_5-3.py
@@ -0,0 +1,6 @@
+print ("This program converts Fahrenheit to Celsius")
+print ("Type in a temperature in Fahrenheit: ",)
+fahrenheit = float(input())           # 从用户那里得到华氏度
+celsius = (fahrenheit - 32) * 5.0 / 9
+print ("That is", celsius, "degrees Celsius" )          
+
